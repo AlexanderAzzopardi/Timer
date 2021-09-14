5491e55ff29c468bcb71ecf3a8d653f269996bc1
Alexander Azzopardi <87972109+AlexanderAzzopardi@users.noreply.github.com>
2021-09-14 14:03:47 +0100

    Add files via upload

---
+++ sample.microservice.timer/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sample.microservice.timer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RedisClear();
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static async void RedisClear()
+        {
+            var client = new DaprClientBuilder().Build();
+            await client.SaveStateAsync("statestore", "timersOnline", 0);
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
